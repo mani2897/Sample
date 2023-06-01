547916a8e8989d7a2523e3a3b26983cab19d5690
mani <mobasesmdswitch@gmail.com>
2023-06-01 10:00:19 +0530

    new fill push

---
+++ src/org/company/CompanyInfo.java
@@ -0,0 +1,23 @@
+package org.company;
+//   companyName(),companyId(),companyAddress()
+public class CompanyInfo {
+	private void companyName() {
+		System.out.println("Manikandan Info Tech");
+	}
+	private void companyId() {
+		System.out.println("Id is 27313");
+	}
+	private void companyAddress() {
+		System.out.println("no.4, valluvar street, chennai");
+				
+	}
+	public static void main(String[] args) {
+		CompanyInfo A=new CompanyInfo();
+		A.companyName();
+		A.companyId();
+		A.companyAddress();
+		
+	}
+	
+
+}
